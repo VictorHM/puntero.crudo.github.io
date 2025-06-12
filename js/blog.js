49760c1bd6ee9a2f15b9fb59fcb9bbc3b3b8deee
Victor Hevia <victor.hevia@chaos.com>
2025-06-12 09:49:58 +0200

    Primer commit. Estructura del blog.

---
+++ js/blog.js
@@ -0,0 +1,19 @@
+fetch('posts.json')
+  .then(response => response.json())
+  .then(posts => {
+    const container = document.getElementById('post-list');
+    posts.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+    posts.forEach(post => {
+      const article = document.createElement('article');
+      article.innerHTML = `
+        <h3><a href="${post.url}">${post.title}</a></h3>
+        <small>${post.date}</small>
+        <p>${post.excerpt}</p>
+      `;
+      container.appendChild(article);
+    });
+  })
+  .catch(error => {
+    console.error("Error cargando posts.json:", error);
+  });
